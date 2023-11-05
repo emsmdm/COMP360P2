f0c6a9c84d3339f189c47845757bbf0fd4381374
mawdogbe <mawdogbe@gmail.com>
2023-11-04 20:00:18 -0400

    more GUI groundwork and updates to AssistantProfessor

--- AssistantProfessor.java
+++ AssistantProfessor.java
@@ -2,16 +2,35 @@ import java.util.*;
 
 public class AssistantProfessor extends Employee {
     protected String extraCourse;
-    protected double summerPay;
+    protected double summerPay = 0.0;
+    protected boolean summerCourse = false;
 
-    protected double additionalPayment(){
-        summerPay = monthly_salary * 1.5;
+    protected double additionalPayment(boolean isTeachingSummerClass){
+        if(isTeachingSummerClass)
+        {
+            summerPay = monthly_salary * 1.5;
+            summerCourse = true;
+        }
         return summerPay;
     }
 
-    public String summerPayment(){
-        String extraPay = String.format("Summer Payment: %s", additionalPayment());
+
+    public String toString()
+    {
+        String extraPay = String.format(super.toString() + "\nSummer Payment: %s", additionalPayment(summerCourse));
         return extraPay;
+    }
+
+    public static void main(String[] args){
+        AssistantProfessor Maw = new AssistantProfessor();
+        Maw.first_name = "Maw";
+        Maw.last_name = "Dogbe";
+        Maw.address = "5217 Emerald Spring Drive";
+        Maw.id = 56789;
+        Maw.monthly_salary = 52000;
+        Maw.additionalPayment(true);
+        System.out.println(Maw.toString());
+    
     }
 
 }
--- PaymentSystemInterface.java
+++ PaymentSystemInterface.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.desktop.SystemSleepEvent;
 
 public class PaymentSystemInterface {
     public JFrame SystemFrame;
@@ -18,11 +19,42 @@ public class PaymentSystemInterface {
 
     public PaymentSystemInterface() {
         SystemFrame = new JFrame("ABC University Payment System");
-        SystemFrame.setSize(350, 400);
+        SystemFrame.setSize(600, 600);
+        SystemFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         gbl = new GridBagLayout();
 		gbc = new GridBagConstraints();
 		SystemFrame.setLayout(gbl);
 		SystemFrame.setVisible(true);
+
+        input = new JPanel();
+		input.setLayout(gbl);
+		output = new JPanel();
+		output.setLayout(gbl);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(3, 3, 3, 3);
+
+        gbc.gridx = 0;
+		gbc.gridy = 0;
+		SystemFrame.add(input, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.ipady = 40;
+		SystemFrame.add(output, gbc);
+
+        Color fieldColor = new Color(91, 200, 95);
+        name = new JTextField(5);
+        name.setBackground(fieldColor);
+
+
+
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 6;
+        gbc.ipady = 0;
+        input.add(name, gbc);
+
     }
 
     public static void main(String[] args) {
